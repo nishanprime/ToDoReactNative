d7a1fc2ab1acded3124b3d79ae45ccf9ab2a968e
nishanprime <thapanishan9@gmail.com>
2022-03-13 14:09:53 -0400

    basic react native to do"

--- App.js
+++ App.js
@@ -1,19 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import {
+  Button,
+  FlatList,
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 export default function App() {
+  const [goal, setEnteredGoal] = useState('');
+  const [goals, setGoals] = useState([]);
+
+  const addToGoals = (goalToAdd) => {
+    setGoals([...goals, goalToAdd]);
+    setEnteredGoal('');
+  };
+  const deleteItem = (itemToDel) => {
+    const newGoals = goals.filter((goal) => goal !== itemToDel);
+    setGoals(newGoals);
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <View style={styles.container}>
+        <TextInput
+          value={goal}
+          style={styles.inputField}
+          placeholder="Add Goals"
+          onChangeText={(text) => setEnteredGoal(text)}
+        />
+        <Button
+          title="Add"
+          onPress={() => {
+            addToGoals(goal);
+          }}
+        />
+      </View>
+
+      {goals.length === 0 ? (
+        <View style={styles.addNewGoals}>
+          <Text>Add new Goals</Text>
+        </View>
+      ) : (
+        <FlatList
+          style={{ height: '80%' }}
+          data={goals}
+          keyExtractor={(item) => item}
+          renderItem={({ item, index }) => (
+            <TouchableOpacity onLongPress={() => deleteItem(item)}>
+              <Text style={styles.listItem}>
+                {index + 1}. {item}
+              </Text>
+            </TouchableOpacity>
+          )}
+        />
+      )}
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: '#fff',
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    width: '100%',
+  },
+  inputField: {
+    borderWidth: 1,
+    padding: 10,
+    flex: 1,
+    borderRadius: 10,
+    margin: 10,
+  },
+  listItem: {
+    paddingHorizontal: 10,
+    paddingVertical: 5,
+    backgroundColor: 'grey',
+    fontSize: 20,
+    fontWeight: 'bold',
+    margin: 10,
+  },
+  addNewGoals: {
+    marginTop: 100,
     alignItems: 'center',
     justifyContent: 'center',
   },
